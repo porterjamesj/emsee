70a5782138cedcb5754b24e8d9a2e6c38903f2fd
James Porter <porterjamesj@gmail.com>
2013-06-10 22:05:05 -0400

    refactored plot into a class, need to figure out a robust way to position the axes labels

--- app.py
+++ app.py
@@ -1,31 +1,11 @@
 #imports
 import sympy as sy
 import numpy as np
+from plot import Plot
 from sympy.parsing.sympy_parser import parse_expr
-import matplotlib.pyplot as plt
 
 from flask import Flask,render_template,request
 app = Flask(__name__)
-
-
-#define x as the symbol we will use.
-x = sy.Symbol('x')
-#y = sy.Symbol('y')
-
-# functions
-def rescale(arr,m):
-    '''Rescale a numpy array so that it runs from 0 to m.'''
-    slid = arr-arr.min()
-    return slid/slid.max() * m
-
-app.vars = {'pheight':300,'pwidth':300,
-            'wheight':400,'wwidth':400,
-            'offset':50}
-
-# define the space over which we solve
-xmax = 10.
-xmin = -5.
-xstep = (xmax-xmin)/(xmax*100)
 
 @app.route('/',methods=['GET'])
 def indexget():
@@ -41,31 +21,12 @@ def indexpost():
     except:
         return render_template('error.html')
 
-    # setup a space to solve over, then evaluate
-    space = np.arange(xmin,xmax,xstep)
-    evals = [(i,exp.evalf(subs={x:i})) for i in space]
+    #make a plot object
 
-    # rescale the results to the size of the plot
-    scaledis = rescale(np.array(zip(*evals)[0]),app.vars['pwidth'])
-    scaledvs = rescale(np.array(zip(*evals)[1]),app.vars['pheight'])
-
-    # apply an offset so we don't run into the edge of the svg
-    scaledis += app.vars['offset']
-    scaledvs += app.vars['offset']
-
-    # generate string for svg and render
-    strevals = ["{0},{1}".format(i,v) for (i,v) in zip(scaledis,scaledvs)]
-    return render_template('return.html',
-                           data=strevals,
-                           wwidth=app.vars['wwidth'],
-                           wheight=app.vars['wheight'],
-                           pheight=app.vars['pheight'],
-                           pwidth=app.vars['pwidth'],
-                           xmax=xmax,
-                           xmin=xmin,
-                           ymax=round(max(zip(*evals)[1])),
-                           ymin=round(min(zip(*evals)[1])),
-                           offset=app.vars['offset'])
+    plot = Plot(exp,(-5,10),(200,200),20)
+    
+    # generate parameters for svg and render
+    return render_template('return.html',plot = plot.svgout())
 
 if __name__ == "__main__":
     app.run(debug = True)

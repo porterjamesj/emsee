487c81378b7240c8072c1b279f41795e22dce796
James Porter <porterjamesj@gmail.com>
2013-08-29 16:37:01 -0400

    pep8

--- app.py
+++ app.py
@@ -1,34 +1,38 @@
 #imports
 import plot as pl
-from flask import Flask,render_template,request,jsonify
+from flask import Flask, render_template, request, jsonify
 app = Flask(__name__)
+
 
 def buildjson(constructor):
     #construct plot object
     try:
-        plot = constructor(request.args.get('eq',type=str),
-                           {i:request.args.get(i,type=float)
-                            for i in request.args if not i in ['eq','dim']})
+        plot = constructor(request.args.get('eq', type=str),
+                           {i: request.args.get(i, type=float)
+                            for i in request.args if not i in ['eq', 'dim']})
     except:
-        return jsonify({"message":"parse error",
-                        "errcode":0})
+        return jsonify({"message": "parse error",
+                        "errcode": 0})
 
     # compute plot data and send back to client
     try:
         plot.evaluate()
     except:
-        return jsonify({"message":"evaluation error",
-                         "errcode":1})
+        return jsonify({"message": "evaluation error",
+                        "errcode": 1})
     plot.sample()
     return jsonify(plot.toclient())
 
-@app.route('/',methods=['GET'])
+
+@app.route('/', methods=['GET'])
 def indexget():
     return render_template('index.html')
+
 
 @app.route('/graph/1d')
 def drawgraph():
     return buildjson(pl.OneDeePlot)
+
 
 @app.route('/graph/2d')
 def drawgraph2d():
@@ -36,4 +40,4 @@ def drawgraph2d():
 
 
 if __name__ == "__main__":
-    app.run(debug = True)
+    app.run(debug=True)
--- plot.py
+++ plot.py
@@ -5,9 +5,10 @@ import sympy as sy
 import emcee
 from sympy.parsing.sympy_parser import parse_expr
 
+
 class Plot:
     """Generic plot class."""
-    def __init__(self,eq,nums):
+    def __init__(self, eq, nums):
         """Initialization requires a string the represents the equation for
         the plot, as well as the plot boudaries, which are xmin and xmax in
         one dimension and ymin and ymax in two dimensions. The boundaries
@@ -25,29 +26,28 @@ class Plot:
         #validate ranges
         if self.xmin >= self.xmax:
             raise RuntimeError("Invalid range.")
-        if hasattr(self,'ymin'):
+        if hasattr(self, 'ymin'):
             if self.ymin >= self.ymax:
                 raise RuntimeError("Invalid range.")
 
 
 class OneDeePlot(Plot):
     """One dimensional plot class."""
-    def evaluate(self,steps=1000):
+    def evaluate(self, steps=1000):
         """Evaluate the given function at each of `step` points
         in the x domain."""
         stepsize = (self.xmax - self.xmin)/steps
         try:
-            self.points = [(i,float(self.exp.evalf(subs={sy.Symbol('x'):i})))
+            self.points = [(i, float(self.exp.evalf(subs={sy.Symbol('x'): i})))
                            for i in np.arange(self.xmin,
                                               self.xmax,
                                               stepsize)]
         except:
-             raise RuntimeError("Evaluation failed.")
+            raise RuntimeError("Evaluation failed.")
         self.ymin = min(zip(*self.points)[1])
         self.ymax = max(zip(*self.points)[1])
 
-
-    def __lnprobfn(self,pos):
+    def __lnprobfn(self, pos):
         """Return the ln of the posterior probability of being in a
         position. The posterior probability is just 1/error^2, where
         error is the difference between the y-value at this position
@@ -55,11 +55,10 @@ class OneDeePlot(Plot):
         if pos[0] > self.xmax or pos[0] < self.xmin:
             # We are outside the acceptable range, probability = 0
             return -np.inf
-        res = float(self.exp.evalf(subs={sy.Symbol('x'):float(pos[0])}))
-        return np.log( 1/((res - self.ymin)**2) )
+        res = float(self.exp.evalf(subs={sy.Symbol('x'): float(pos[0])}))
+        return np.log(1/((res - self.ymin)**2))
 
-
-    def sample(self,nsamples=1000,vardiv=10):
+    def sample(self, nsamples=1000, vardiv=10):
         """Generate a Markov chain of length `nsamples`
 
         vardim controls the variance of the proposal distribution, which
@@ -67,26 +66,27 @@ class OneDeePlot(Plot):
         cause small jumps and vice versa."""
 
         var = (self.xmax - self.xmin) / vardiv
-        sampler = emcee.MHSampler([[var,var],[var,var]], #cov
-                                  2, #dim
+        sampler = emcee.MHSampler([[var, var], [var, var]],  # cov
+                                  2,  # dim
                                   self.__lnprobfn)
 
-        initval = float(np.random.uniform(self.xmin,self.xmax,1))
-        res = sampler.run_mcmc([initval,initval],nsamples)
+        initval = float(np.random.uniform(self.xmin, self.xmax, 1))
+        sampler.run_mcmc([initval, initval], nsamples)
 
         self.chain = zip(*sampler.chain)[0]
 
     def toclient(self):
         """Return a dictionary of everything that needs to be passed
         to the client in order for plotting to take place."""
-        return {"points" : self.points,
-                "xmin" : self.xmin,
-                "xmax" : self.xmax,
-                "chain" : self.chain}
+        return {"points": self.points,
+                "xmin": self.xmin,
+                "xmax": self.xmax,
+                "chain": self.chain}
+
 
 class TwoDeePlot(Plot):
     """Subclass for a two dimensional plot."""
-    def evaluate(self,xsteps=100,ysteps=100):
+    def evaluate(self, xsteps=100, ysteps=100):
         """Evaluate the plot's equation at a set of points determined
         by the number of x and y steps passed in."""
 
@@ -97,51 +97,51 @@ class TwoDeePlot(Plot):
         self.xs = list(np.arange(self.xmin, self.xmax, stepsizex))
         self.ys = list(np.arange(self.ymin, self.ymax, stepsizey))
 
-        self.zs = [[float(self.exp.evalf(subs={sy.Symbol('x'):i,
-                                          sy.Symbol('y'):j}))
+        self.zs = [[float(self.exp.evalf(subs={sy.Symbol('x'): i,
+                                               sy.Symbol('y'): j}))
                     for j in self.ys]
-                    for i in self.xs]
+                   for i in self.xs]
 
         # get min and max z value
         self.zmin = min([min(row) for row in self.zs])
         self.zmax = max([max(row) for row in self.zs])
 
-    def __lnprobfn(self,pos):
+    def __lnprobfn(self, pos):
         """Returns theln of the posterior probability of any point
         in the two dimensional space. This is just 1/error^2, where
         error is the difference between f(pos) and the minimum z value."""
         if (pos[0] > self.xmax or pos[0] < self.xmin or
-            pos[1] > self.ymax or pos[1] < self.ymin):
+                pos[1] > self.ymax or pos[1] < self.ymin):
             return -np.inf
-        res = float(self.exp.evalf(subs={sy.Symbol('x'):float(pos[0]),
-                                         sy.Symbol('y'):float(pos[1])}))
-        return np.log( 1/((res-self.zmin)**2) )
+        res = float(self.exp.evalf(subs={sy.Symbol('x'): float(pos[0]),
+                                         sy.Symbol('y'): float(pos[1])}))
+        return np.log(1/((res-self.zmin)**2))
 
-    def sample(self,nsamples=1000,vardivx=10,vardivy=10):
+    def sample(self, nsamples=1000, vardivx=10, vardivy=10):
         """Generate a Markov chain of length `nsamples`"""
         xvar = (self.xmax-self.xmin)/vardivx
         yvar = (self.ymax-self.ymin)/vardivy
-        cov = np.array([[xvar,0],
-                        [0,yvar]])
+        cov = np.array([[xvar, 0],
+                        [0, yvar]])
 
         sampler = emcee.MHSampler(cov, 2, self.__lnprobfn)
 
         initstate = np.concatenate((
-            np.random.uniform(self.xmin,self.xmax,1),
-            np.random.uniform(self.ymin,self.ymax,1)
+            np.random.uniform(self.xmin, self.xmax, 1),
+            np.random.uniform(self.ymin, self.ymax, 1)
         ))
 
-        res = sampler.run_mcmc(initstate,nsamples)
+        sampler.run_mcmc(initstate, nsamples)
 
         self.chain = sampler.chain.tolist()
 
     def toclient(self):
         """Package up everything we need to send to the client in a dict."""
-        return {"zs" : self.zs,
-                "xmin" : self.xmin,
-                "xmax" : self.xmax,
-                "ymin" : self.ymin,
-                "ymax" : self.ymax,
-                "zmin" : self.zmin,
-                "zmax" : self.zmax,
-                "chain" : self.chain}
+        return {"zs": self.zs,
+                "xmin": self.xmin,
+                "xmax": self.xmax,
+                "ymin": self.ymin,
+                "ymax": self.ymax,
+                "zmin": self.zmin,
+                "zmax": self.zmax,
+                "chain": self.chain}

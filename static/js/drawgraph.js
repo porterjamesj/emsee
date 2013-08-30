68c63a27f6aa5de4e42ad7eadfff6da43270787f
James Porter <porterjamesj@gmail.com>
2013-08-29 23:31:52 -0400

    Excise jQuery :)

--- static/js/drawgraph.js
+++ static/js/drawgraph.js
@@ -1,63 +1,59 @@
 ;(function (exports) {
-  /*
-   * Utility function to flash an error div on a given selection.
-   */
-  var errorctrl = function(element,msg) {
-    if(errorctrl.lastrun + 2500 < new Date().getTime()) {
-      errorctrl.lastrun = new Date().getTime();
-      var $element =$(element.node());
-      $element.children("#error")
-        .text(msg)
-        .fadeIn(500)
-        .delay(1500)
-        .fadeOut(500);
-    }
+
+  /* http://stackoverflow.com/questions/1714786/ */
+  var serialize = function(obj) {
+    var str = [];
+    for(var p in obj)
+      str.push(encodeURIComponent(p) + "=" + encodeURIComponent(obj[p]));
+    return str.join("&");
   };
-  errorctrl.lastrun = 0;
+
+
+  /* Utility function to flash an error div on a given selection. */
+  var errorctrl = function(element,msg) {
+    element.select(".error")
+      .style("display","block")
+      .text(msg)
+      .classed("animated fadeIn",true);
+  };
 
   var getInputOneDee = function() {
-    var tab = $("#onedee");
-    var data = {eq: tab.find('input[name="equation"]').val(),
-                xmin: tab.find('input[name="xmin"]').val(),
-                xmax: tab.find('input[name="xmax"]').val(),
+    var tab = d3.select("#onedee");
+    var data = {eq: tab.select('input[name="equation"]').node().value,
+                xmin: tab.select('input[name="xmin"]').node().value,
+                xmax: tab.select('input[name="xmax"]').node().value,
                 dim: 1};
     return data;
   };
 
   var getInputTwoDee = function() {
-    var tab = $("#twodee");
-    var data = {eq: tab.find('input[name="equation"]').val(),
-                xmin: tab.find('input[name="xmin"]').val(),
-                xmax: tab.find('input[name="xmax"]').val(),
-                ymin: tab.find('input[name="ymin"]').val(),
-                ymax: tab.find('input[name="ymax"]').val(),
+    var tab = d3.select("#twodee");
+    var data = {eq: tab.select('input[name="equation"]').node().value,
+                xmin: tab.select('input[name="xmin"]').node().value,
+                xmax: tab.select('input[name="xmax"]').node().value,
+                ymin: tab.select('input[name="ymin"]').node().value,
+                ymax: tab.select('input[name="ymax"]').node().value,
                 dim: 2};
     return data;
   };
 
   var sendAjax = function(element,url,data,loader) {
-    this.jqxhr = $.ajax({
-      dataType: "json",
-      url: $SCRIPT_ROOT + url,
-      data: data,
-      success: function(data) {
-        callback(element,data); /* element, so we know where to put the plot. */
-      },
-      beforeSend: function() { loader.addClass("loadingon"); },
-      complete: function() { loader.removeClass("loadingon"); }
+    loader.classed("loadingon",true);
+    d3.json($SCRIPT_ROOT + url + "?" + serialize(data), function(err,data) {
+      callback(element, data, err);
     });
-
   };
 
-  var callback = function(element,data) {
+  var callback = function(element,data,err) {
     /* Handles DOM updates. */
-    $(element.node()).children("svg").remove(); /* Get rid of the old plot. */
+    element.select("svg").remove(); /* Get rid of the old plot. */
+    element.select(".error").style("display","none"); /* Hide error message. */
 
     /* Handle errrors. */
-    if (data.errcode === 0) { //This was a parse error
+    if (err && err.status === 400) { //This was a parse error
       errorctrl(element,"Errr, sorry, I couldn't parse that.");
       return;
-    } else if (data.errcode === 1) { //This was an evaluation error
+    } else if (err && err.status === 406) { //This was an evaluation error
       errorctrl(
         element,
         "There was an error evaluating your function, possibly a complex number"
@@ -84,7 +80,7 @@
     var sendOneDeeAjax = function() {
       /* Get the data from the DOM. */
       var data = getInputOneDee();
-      var loader = $('#load1');
+      var loader = d3.select('#load1');
       var url = '/graph/1d';
       var element = d3.select('#onedee');
       /* Make an ajax call. */
@@ -94,36 +90,40 @@
     var sendTwoDeeAjax = function() {
       /* Get the data from the DOM. */
       var data = getInputTwoDee();
-      var loader = $('#load2');
+      var loader = d3.select('#load2');
       var url = '/graph/2d';
       var element = d3.select('#twodee');
       /* Make an ajax call. */
       sendAjax(element,url,data,loader);
     };
 
-    $('button#submit.onedee').click(sendOneDeeAjax);
-    $('button#submit.twodee').click(sendTwoDeeAjax);
-    $('input.onedee').keydown(
-      function (e) {if (e.keyCode === 13) {sendOneDeeAjax();}});
-    $('input.twodee').keydown(
-      function (e) {if (e.keyCode === 13) {sendTwoDeeAjax();}});
+    d3.select('button#submit.onedee').on("click",sendOneDeeAjax);
+    d3.select('button#submit.twodee').on("click",sendTwoDeeAjax);
+    d3.selectAll('input.onedee')
+      .on("keydown", function () {
+        if (d3.event.keyCode === 13) {sendOneDeeAjax();}
+      });
+    d3.selectAll('input.twodee')
+      .on("keydown", function (e) {
+        if (d3.event.keyCode === 13) {sendTwoDeeAjax();}
+      });
 
     /*
      * Bind key events in the equation input fields to changing the appropriate
      * math rendering <p>
      */
-    $('input[name=equation]').keyup(
-      function() {
-        var self = $(this);
-        var rend = self.parent().siblings('.render');
-        rend.text("`" + self.val() + "`");
-        MathJax.Hub.Queue(["Typeset",MathJax.Hub,rend.get(0)]);
+    d3.selectAll('input[name=equation]')
+      .on("keyup", function() {
+        var rend = d3.select(this.parentNode.parentNode).select(".render");
+        rend.text("`" + this.value + "`");
+        console.log(rend.node());
+        MathJax.Hub.Queue(["Typeset",MathJax.Hub,rend.node()]);
       });
 
   };
 
 })(this);
 
-$(function () {
+window.addEventListener('load', function () {
   addEventListeners();
 });

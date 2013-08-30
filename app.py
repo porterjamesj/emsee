9e022e718546589668fa2299e7e217acbf62939d
James Porter <porterjamesj@gmail.com>
2013-08-29 23:29:15 -0400

    Use HTTP status codes rather than JSON error messages.

--- app.py
+++ app.py
@@ -11,15 +11,13 @@ def buildjson(constructor):
                            {i: request.args.get(i, type=float)
                             for i in request.args if not i in ['eq', 'dim']})
     except:
-        return jsonify({"message": "parse error",
-                        "errcode": 0})
+        return "", 400
 
     # compute plot data and send back to client
     try:
         plot.evaluate()
     except:
-        return jsonify({"message": "evaluation error",
-                        "errcode": 1})
+        return "", 406
     plot.sample()
     return jsonify(plot.toclient())
 
@@ -29,12 +27,12 @@ def indexget():
     return render_template('index.html')
 
 
-@app.route('/graph/1d')
+@app.route('/graph/1d', methods=["GET"])
 def drawgraph():
     return buildjson(pl.OneDeePlot)
 
 
-@app.route('/graph/2d')
+@app.route('/graph/2d', methods=["GET"])
 def drawgraph2d():
     return buildjson(pl.TwoDeePlot)
 
